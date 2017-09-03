25b7671c585811e8f18f287cec750bb672d77233
Yang Yang <yyang.even@gmail.com>
2017-09-03 11:28:06 -0700

    Vim: Enable cscope
    1. Auto-load cscope database.
    2. Reorganize vimrc.
    3. Add script setup_cscope_for.sh.
    4. Add cscope package to vim install script.
    
    Change-Id: I4e4021c1e4ae874f2aa6d68d8de00456ef297bc8

---
+++ scripts/setup_cscope_for.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+WORKING_DIR=$(pwd)
+PROJECT_ROOT_DIR=$WORKING_DIR
+CPP_FILES_FILE=/tmp/cpp.cscope
+
+# Use the first command argument if possible,
+# Use current working directory otherwise
+if [ $# -eq 1 ]; then
+    if [ -d "$1" ]; then
+        PROJECT_ROOT_DIR="$1"
+    fi
+fi
+
+echo "Building cscope database for \""$PROJECT_ROOT_DIR"\"."
+
+#Find all CPP files
+pushd /
+find "$PROJECT_ROOT_DIR" -name '*.c' -or -name '*.h' -or -name '*.hpp' -or -name '*.cpp' -or -name '*.cc' > "$CPP_FILES_FILE"
+popd
+
+#Build cscope database
+#   reference: http://cscope.sourceforge.net/large_projects.html
+pushd "$PROJECT_ROOT_DIR"
+cscope -b -q -i "$CPP_FILES_FILE"
+popd
--- scripts/vim.sh
+++ scripts/vim.sh
@@ -5,3 +5,4 @@ source util.sh
 Install_Package_If_Necessary vim
 Install_Package_If_Necessary vim-gnome
 Install_Package_If_Necessary exuberant-ctags
+Install_Package_If_Necessary cscope
