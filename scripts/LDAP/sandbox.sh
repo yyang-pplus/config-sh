6af30ed167c6e37f2d671b8a35dba243914e53c5
Yang Yang <yang.yang@netscout.com>
2018-10-17 16:29:59 -0700

    Scripts: Add LDAP/sandbox.sh
    Change-Id: Ifc07c8f52129aed6f92c07d1e8c70a9c9e1370d3

---
+++ scripts/LDAP/sandbox.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is only for CentOS.
+
+
+source scripts/util.sh
+
+
+sudo yum -y install openldap-servers openldap-clients
+
+if which systemctl &> /dev/null; then
+    Log_Run sudo systemctl enable slapd
+    Log_Run sudo systemctl start slapd
+    sudo systemctl status slapd
+else
+    Log_Run sudo chkconfig slapd on
+    sudo chkconfig --list slapd
+
+    sudo service slapd start
+    sudo service slapd status
+fi
