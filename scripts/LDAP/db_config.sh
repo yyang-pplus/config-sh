09d11529c4c55f35bbe4244455ae178a8f54f746
Yang Yang <yang.yang@netscout.com>
2018-12-18 10:17:36 -0800

    Scripts: LDAP: Replace replace_suffix.sh with db_config.sh
    Change-Id: Id2de32687ccc934c1b77e0303a20c37468ca9073

---
+++ scripts/LDAP/db_config.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This script config olcSuffix and olcRootDN to the desired value. It also prompt to set olcRootPW.
+#   It determines the DN of the database automatically if there is only one DB.
+
+SUFFIX="dc=yyangtech,dc=wordpress,dc=com"
+DB_DN=$(sudo ldapsearch -H ldapi:// -Y EXTERNAL -b "cn=config" -s one "(olcSuffix=*)" dn -LLL -Q)
+NUMBER_DB=$(wc -l <<< "$DB_DN")
+
+echo "Running:" $(basename "$0")
+
+if [ "$NUMBER_DB" -ne 1 ]; then
+    echo "Error: Multiple DB:"
+    echo $DB_DN
+    exit 1
+fi
+
+PASSWORD=$(slappasswd -h {SSHA})
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+# echo the suffix before change
+sudo ldapsearch -H ldapi:// -Y EXTERNAL -b "cn=config" -s one "(olcSuffix=*)" olcSuffix olcRootDN olcRootPW -LLL -Q
+
+sudo ldapmodify -Y EXTERNAL  -H ldapi:// -Q <<EOF
+$DB_DN
+changetype: modify
+replace: olcSuffix
+olcSuffix: $SUFFIX
+
+$DB_DN
+changetype: modify
+replace: olcRootDN
+olcRootDN: cn=admin,$SUFFIX
+
+$DB_DN
+changetype: modify
+replace: olcRootPW
+olcRootPW: $PASSWORD
+EOF
+
+# echo the suffix after change
+sudo ldapsearch -H ldapi:// -Y EXTERNAL -b "cn=config" -s one "(olcSuffix=*)" olcSuffix olcRootDN olcRootPW -LLL -Q
