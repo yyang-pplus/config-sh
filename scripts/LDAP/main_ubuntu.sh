a9d32c6a8efe31d23001c985e7f914945b653aab
Yang Yang <yyang.pplus@gmail.com>
2018-12-31 12:05:09 -0800

    LDAP: Add support for Ubuntu
    Change-Id: I8fd776a2aa4ecd98998f72fd268abc3a94a03c2f

---
+++ scripts/LDAP/main_ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script is only for Ubuntu.
+
+
+THIS_DIR=$(dirname "$0")
+
+
+sudo apt install slapd ldap-utils
+# Config DB
+sudo dpkg-reconfigure slapd
+
+source "$THIS_DIR/global_defines.sh"
+
+# This script only works for single normal DIT configuration
+if [ "$NUMBER_DB" -ne 1 ]; then
+    echo "Error: Multiple DB:"
+    echo $DB_DN
+    exit 1
+fi
+
+# Now we should have only one module config entry
+if [ "$NUMBER_MODULE_CONFIG" -ne 1 ]; then
+    echo "Error: Multiple module config entries:"
+    echo $MODULE_CONFIG_DN
+    exit 1
+fi
+
+./$THIS_DIR/overlay_memberof.sh
+./$THIS_DIR/overlay_refint.sh
+
+# Query current overlay configuration
+sudo ldapsearch -H ldapi:// -Y EXTERNAL -b "cn=config" -LLL -Q "objectClass=olcOverlayConfig"
