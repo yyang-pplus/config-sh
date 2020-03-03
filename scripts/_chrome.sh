a1c7d21de29a8135ebd751a881f9158c549f0aa8
Yang Yang <yang.yang@netscout.com>
2020-03-03 15:12:27 -0800

    Scripts: Add CentOS support to _chrome.sh
    Change-Id: Ied5db5fad360b1f9825a4016bafd7311fb5bcba1

--- scripts/_chrome.sh
+++ scripts/_chrome.sh
@@ -7,11 +7,24 @@ source "$THIS_DIR/util.sh"
 ##
 # @reference    How to Install Google Chrome Web Browser on Ubuntu 18.04
 #               https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-ubuntu-18-04/
+# @reference    How to Install Google Chrome Web Browser on CentOS 7
+#               https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-centos-7/
 ##
 temp_dir=$(date +%s)
 mkdir $temp_dir
 pushd $temp_dir
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    Install_Packages ./google-chrome-stable_current_amd64.deb
+    if [ -f /etc/debian_version ]; then
+        PACKAGE_NAME="google-chrome-stable_current_amd64.deb"
+    else
+        PACKAGE_NAME="google-chrome-stable_current_x86_64.rpm"
+    fi
+
+    wget "https://dl.google.com/linux/direct/$PACKAGE_NAME"
+
+    if [ -f /etc/debian_version ]; then
+        Install_Packages "./$PACKAGE_NAME"
+    else
+        sudo yum --assumeyes localinstall "$PACKAGE_NAME"
+    fi
 popd
 rm -rf "$temp_dir"
