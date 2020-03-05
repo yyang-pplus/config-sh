072347713f304bf6478dcb197006a632cb99bb00
Yang Yang <yyang.even@gmail.com>
2020-03-05 14:37:08 -0800

    Scripts: Fixing _virtualbox_guest_additions.sh
    Change-Id: Iaa6bcb247363676ef4b0c0c601295982418eef07

--- scripts/_virtualbox_guest_additions.sh
+++ scripts/_virtualbox_guest_additions.sh
@@ -5,16 +5,21 @@ THIS_DIR=$(dirname "$0")
 source "$THIS_DIR/util.sh"
 
 
+echo "Fixing VM shared folder permission issue."
+sudo usermod -aG vboxsf $(whoami)
+sudo grep "vboxsf" /etc/group
+
+
 ##
 # @reference    How to Install VirtualBox Guest Additions on Ubuntu 18.04
 #               https://linuxize.com/post/how-to-install-virtualbox-guest-additions-in-ubuntu/
 ##
+echo "Installing VirtualBox Guest Additions."
 Install_Packages build-essential dkms linux-headers-$(uname -r)
 
-sudo updatedb
-
-installer_path=$(locate VBoxLinuxAdditions.run)
+installer_path=$(find /media/ -name "VBoxLinuxAdditions.run")
 if [ $(wc -l <<< "$installer_path") -eq 1 ]; then
+    echo "Running installer '$installer_path'"
     sudo sh "$installer_path"
 else
     Echo_Error "Installer not found."
