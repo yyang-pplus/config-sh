e6e0bab1d9b431435d9bf3a7f61875c0c073dccd
Yang Yang <yyang.pplus@gmail.com>
2020-08-24 12:49:10 -0700

    Common: Some improvements for Ubuntu 20.04
    Change-Id: I28706a1bf3c082675ab23308a12e54b6a27930ef

--- scripts/_vim.sh
+++ scripts/_vim.sh
@@ -5,7 +5,7 @@ source "$THIS_DIR/util.sh"
 
 
 REDHAT_PACKAGES_LIST="vim-X11 ctags"
-DEBIAN_PACKAGES_LIST="vim-gnome exuberant-ctags"
+DEBIAN_PACKAGES_LIST="vim-gtk3 exuberant-ctags"
 COMMON_PACKAGES_LIST="cscope"
 
 redhat_packages_list=$REDHAT_PACKAGES_LIST debian_packages_list=$DEBIAN_PACKAGES_LIST Install_Packages $COMMON_PACKAGES_LIST
