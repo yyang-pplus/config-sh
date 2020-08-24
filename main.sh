28d966ec73bb6b3b63c4ffe8dbd2e9d7c085eb28
Yang Yang <yyang.even@gmail.com>
2020-08-24 12:49:10 -0700

    Common: Some improvements for Ubuntu 20.04
    Change-Id: I28706a1bf3c082675ab23308a12e54b6a27930ef

--- main.sh
+++ main.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-source ~/.bash_util.sh
 
+##
+# @note	Avoid using bash_util.sh functions in this script.
 
 # Don't run this script as sudo, as required by git.sh
 if [ "$EUID" -eq 0 ]; then
-    Fatal "Cannot run as root."
+    echo "Cannot run as root."
+    exit 1
 fi
+
+
+set -ex
 
 
 MAIN_DIR=$(dirname "$0")
@@ -14,14 +19,15 @@ SCRIPT_DIR=$MAIN_DIR/scripts
 
 
 # Run selected scripts
-QuietRun pushd $SCRIPT_DIR
+pushd $SCRIPT_DIR
+    printf "\n\n"
+    # config.sh needs to run first to setup bash_util.sh.
+    ./config.sh
+
     SELECTED_SCRIPTS=( $(ls _*) )
 
     for script in "${SELECTED_SCRIPTS[@]}"; do
         printf "\n\nRunning script: $script\n"
         ./$script
     done
-
-    printf "\n\n"
-    ./config.sh
-QuietRun popd
+popd
--- setup_linux.sh
+++ setup_linux.sh
@@ -6,6 +6,9 @@
 ##
 
 
+set -ex
+
+
 if [ "$EUID" -eq 0 ]; then
     echo "Error: Cannot run as root."
     exit 1
@@ -18,6 +21,10 @@ fi
 #
 printf "\nGenerating a new SSH key.\n"
 read -p 'SSH Key ID (user@os.machine.domain): ' key_id
+if [ -z "${key_id// }" ]; then
+    echo "Error: Please set SSH Key ID."
+    exit 1
+fi
 ssh-keygen -t rsa -b 4096 -C "$key_id"
 
 ssh-add $HOME/.ssh/id_rsa
@@ -34,26 +41,26 @@ sudo apt --yes update
 sudo apt --yes upgrade
 
 
+TMP_DIR="$HOME/tmp"
+mkdir "$TMP_DIR"
 sudo apt --yes install git
 PROJECTS_DIR="$HOME/projects"
 mkdir "$PROJECTS_DIR"
-
-
 pushd "$PROJECTS_DIR"
     ACTIVE_PROJECTS=(yyLinuxConfig yyLinuxScripts algorithms yyangtech_wordpress_com)
     for a_project in ${ACTIVE_PROJECTS[@]}; do
         printf "\nDownloading project $a_project\n"
-        git clone "ssh://yyang-even@review.gerrithub.io:29418/yyang-even/$a_project" && scp -p -P 29418 yyang-even@review.gerrithub.io:hooks/commit-msg "$a_project/.git/hooks/"
+        git clone --recurse-submodules -j8 "ssh://yyang-even@review.gerrithub.io:29418/yyang-even/$a_project" && scp -p -P 29418 yyang-even@review.gerrithub.io:hooks/commit-msg "$a_project/.git/hooks/"
     done
 
 
     pushd yyLinuxConfig
-        ./main.sh
+        ./main.sh | tee "$TMP_DIR/config.txt"
     popd
 
 
     pushd algorithms
         printf "\n\n"
-        ./scripts/setup.sh
+        ./scripts/setup.sh | tee "$TMP_DIR/algorithms.txt"
     popd
 popd
