422d67839d9d5c1a9287a4d02fe1cb164b7e8e50
Yang Yang <yyang.even@gmail.com>
2020-09-04 22:37:50 -0700

    Scripts: Don't run virtualbox.sh automatically
    Change-Id: If7288cd28f1088bee43a0a3c95de62b2a0a18d91

--- scripts/config.sh
+++ scripts/config.sh
@@ -21,7 +21,7 @@ for config_name in "${SUPPORT_CONFIGS[@]}"; do
     if [ -e "$home_config" ]; then
         if [ ! -L "$home_config" ]; then
             echo "Backup existing configuration file to: $backup"
-            cp -r "$home_config" "$backup"
+            mv "$home_config" "$backup"
         fi
     fi
 
