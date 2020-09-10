52c1373c679c89518734f3f53e09a8bf1016d0cc
Yang Yang <yyang.pplus@gmail.com>
2020-09-10 11:33:12 -0700

    Vim: Add plugin vim-test
    Change-Id: Ia6817ccb839fbf06b7ba0f1140a633c4a8d805d4

--- scripts/_python.sh
+++ scripts/_python.sh
@@ -4,7 +4,7 @@ THIS_DIR=$(dirname "$0")
 source "$THIS_DIR/util.sh"
 
 
-Install_Packages python3-pip
+Install_Packages python3-pip python-is-python3
 
 sudo pip3 install pipenv
 sudo pip3 install black
