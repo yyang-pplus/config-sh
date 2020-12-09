a9c418f7c168ee4e4e7e6a1bb5966143e6a3d04f
Yang Yang <yyang.even@gmail.com>
2020-12-09 13:03:45 -0800

    CI: Try fixing the test on Travis
    Change-Id: I860093098cb75cf0b9fcc17a121013c4a7330ee2

--- tests/pre-commit_test.sh
+++ tests/pre-commit_test.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# set -ex
+set -ex
 
 pre-commit install
 
 pre-commit autoupdate
 
 pre-commit run --all-files
-
-# This test is disabled because of the following pip error on Travis:
-#   ERROR: pre-commit 2.7.1 has requirement virtualenv>=20.0.8, but you'll have virtualenv 15.1.0 which is incompatible.
-exit 0
