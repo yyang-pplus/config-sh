044981a24383c405a8c4f073bfbc11a32ce28aa9
Yang Yang <yyang.pplus@gmail.com>
2022-03-25 00:12:35 -0700

    Main: Use bash instead of sh

--- setup_ubuntu_vbox.sh
+++ setup_ubuntu_vbox.sh
@@ -9,7 +9,7 @@ set -e
 
 pushd /tmp
 SCRIPT_NAME="setup_ubuntu.sh"
-wget -O $SCRIPT_NAME "https://raw.githubusercontent.com/yyang-pplus/config-sh/master/$SCRIPT_NAME" && sh $SCRIPT_NAME
+wget -O $SCRIPT_NAME "https://raw.githubusercontent.com/yyang-pplus/config-sh/master/$SCRIPT_NAME" && bash $SCRIPT_NAME
 popd
 
 $HOME/projects/config-sh/scripts/virtualbox.sh
