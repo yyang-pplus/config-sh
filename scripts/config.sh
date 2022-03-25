07318231ea5de400c4f85bcc7f96f2b711e4ada3
Yang Yang <yyang.pplus@gmail.com>
2022-03-25 16:44:45 -0700

    Config: Support nested config files such as terminator config

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,23 +5,29 @@ set -e
 THIS_DIR=$(dirname "$0")
 
 CONFIG_DIR=$(readlink --canonicalize "$THIS_DIR/../config")
-BACKUP_DIR=$HOME/$(date +%s)
+BACKUP_ROOT_DIR=$HOME/$(date +%s)
 SUPPORT_CONFIGS=($(ls "$CONFIG_DIR"))
 
-mkdir -p "$BACKUP_DIR"
+mkdir -p "$BACKUP_ROOT_DIR"
 
-alias_backup=$BACKUP_DIR/.alias
+alias_backup=$BACKUP_ROOT_DIR/.alias
 echo "Backup existing alias to: $alias_backup"
 alias &> "$alias_backup"
 
 for config_name in "${SUPPORT_CONFIGS[@]}"; do
-    home_config=$HOME/.$config_name
-    backup=$BACKUP_DIR/.$config_name
     new_config=$CONFIG_DIR/$config_name
+
+    # Expand '@' file path format to '/' directory path format
+    config_name=${config_name//@//}
+    home_config=$HOME/.$config_name
+    backup=$BACKUP_ROOT_DIR/.$config_name
+
+    mkdir -p $(dirname "$home_config")
 
     if [ -e "$home_config" ]; then
         if [ ! -L "$home_config" ]; then
             echo "Backup existing configuration file to: $backup"
+            mkdir -p $(dirname "$backup")
             mv "$home_config" "$backup"
         fi
     fi
@@ -29,7 +35,7 @@ for config_name in "${SUPPORT_CONFIGS[@]}"; do
     ln --symbolic --force --no-target-directory "$new_config" "$home_config"
 done
 
-OLD_BASH_RC=$BACKUP_DIR/.bashrc
+OLD_BASH_RC=$BACKUP_ROOT_DIR/.bashrc
 if [ -f "$OLD_BASH_RC" ]; then
     LOCAL_BASH_RC=$HOME/.bashrc.local
     if [ ! -f "$LOCAL_BASH_RC" ]; then
