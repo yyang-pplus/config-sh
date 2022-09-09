b9d63919d9248b4cf1949cd0104111d5e0d06f05
Yang Yang <yyang.pplus@gmail.com>
2022-09-09 13:31:43 -0700

    Scripts: Add _wallpaper.sh

---
+++ scripts/_wallpaper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$0")
+source "$THIS_DIR/util.sh"
+
+if isRedHat; then
+    echo "Setting wallpaper only works on Ubuntu for now."
+    exit 0
+fi
+
+if ! which xdpyinfo &> /dev/null; then
+    echo "Skip setting wallpaper: Not desktop."
+    exit 0
+fi
+
+RESOLUTION=$(xdpyinfo | grep -oP 'dimensions:\s+\K\S+')
+
+FILENAME="vim-shortcuts-blue-1920x1200.png"
+if [[ $RESOLUTION == *"2560x1600"* ]]; then
+    FILENAME="vim-shortcuts-dark_2560x1600.png"
+fi
+
+URI="https://raw.githubusercontent.com/LevelbossMike/vim_shortcut_wallpaper/master/$FILENAME"
+
+wget --no-check-certificate -O $HOME/Pictures/$FILENAME "$URI"
+
+gsettings set org.gnome.desktop.background picture-uri "file:///$HOME/Pictures/$FILENAME"
