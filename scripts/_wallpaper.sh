d27d56972f47c732ccbceb9b4bc8dd53d8c9eb8c
Yang Yang <yyang.pplus@gmail.com>
2022-09-10 11:07:59 -0700

    Scripts: Support more resolution for setting wallpaper

--- scripts/_wallpaper.sh
+++ scripts/_wallpaper.sh
@@ -20,6 +20,8 @@ RESOLUTION=$(xdpyinfo | grep -oP 'dimensions:\s+\K\S+')
 FILENAME="vim-shortcuts-blue-1920x1200.png"
 if [[ $RESOLUTION == *"2560x1600"* ]]; then
     FILENAME="vim-shortcuts-dark_2560x1600.png"
+elif [[ $RESOLUTION == *"2048x1280"* ]]; then
+    FILENAME="vim-shortcuts-dark_2560x1600.png"
 fi
 
 URI="https://raw.githubusercontent.com/LevelbossMike/vim_shortcut_wallpaper/master/$FILENAME"
