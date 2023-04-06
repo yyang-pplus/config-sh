9b2eca8058e7fdce62940da603dbe29b30dfe063
Yang Yang <yyang.pplus@gmail.com>
2023-04-06 12:13:13 -0700

    Scripts: Add docker.sh

---
+++ scripts/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+##
+# @reference    Install Docker Engine on Ubuntu
+#               https://docs.docker.com/engine/install/ubuntu/
+##
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+if which docker &> /dev/null; then
+    echo "Docker already installed."
+    exit 0
+fi
+
+pushd /tmp
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+popd
+
+sudo usermod -aG docker $USER
