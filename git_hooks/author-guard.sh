7df13d63eae86dce43ad2b82bfd963401de3c864
Yang Yang <yyang.pplus@gmail.com>
2023-05-20 13:46:27 -0700

    Scripts: Minor improvement for Oracle Linux

--- git_hooks/author-guard.sh
+++ git_hooks/author-guard.sh
@@ -6,11 +6,11 @@ EXPECTED_NAME=$(git config --global user.name)
 
 if git remote -v | grep "yyang-pplus" > /dev/null; then
     EXPECTED_EMAIL_SUBSTR="pplus"
-else
+elif git remote -v | grep "yyang-even" > /dev/null; then
     EXPECTED_EMAIL_SUBSTR="even"
 fi
 
-if [[ "$GIT_AUTHOR_EMAIL" != *"$EXPECTED_EMAIL_SUBSTR@"* ]]; then
+if [[ "$GIT_AUTHOR_EMAIL" != *"$EXPECTED_EMAIL_SUBSTR@"* ]] || [[ -z "$GIT_AUTHOR_EMAIL" ]]; then
     echo "Unexpected user email. Expect: '$EXPECTED_EMAIL_SUBSTR'"
     echo "Use following command to fix it:"
     echo "    git config --local user.email 'yyang@example.com'"
