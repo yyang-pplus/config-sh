eb93794d5be3fa2a605d9434f4ef5ff8e4c89d20
Yang Yang <yyang.pplus@gmail.com>
2024-02-27 15:01:20 -0800

    Main: Use absolute path in main.sh

--- main.sh
+++ main.sh
@@ -12,11 +12,12 @@ fi
 set -e
 
 MAIN_DIR=$(dirname "$0")
+MAIN_DIR=$(realpath "$MAIN_DIR")
 SCRIPT_DIR=$MAIN_DIR/scripts
 
-./$SCRIPT_DIR/config.sh
-./$SCRIPT_DIR/dependencies.sh
+$SCRIPT_DIR/config.sh
+$SCRIPT_DIR/dependencies.sh
 
 source "$HOME/.bashrc"
 
-./$SCRIPT_DIR/install_all_hooks.sh "$MAIN_DIR/.."
+$SCRIPT_DIR/install_all_hooks.sh "$MAIN_DIR/.."
